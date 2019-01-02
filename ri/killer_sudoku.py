1673ff1cbcfbb0ea4a84e76e1e7da44a188f878d
Riyansh Karani <riyansh.karani.011235@gmail.com>
2019-01-02 22:14:26 +0530

    removed use of available values, because it did not make sense, optimized runtime

--- ri/killer_sudoku.py
+++ ri/killer_sudoku.py
@@ -1,6 +1,7 @@
 import copy
 import argparse
 from time import time
+from math import ceil
 
 class KillerSudokuSolver:
     def __init__(self):
@@ -9,25 +10,20 @@ class KillerSudokuSolver:
     def __are_no_numbers_available(self, _available_numbers):
         return sum(_available_numbers) == 0
 
-    def get_factors(self, _sum, _num, _available_numbers=None):
-        assert _sum > 0 and _num > 0
+    def get_factors(self, sum_, num):
+        assert sum_ > 0 and num > 0
 
-        # TODO(riyansh): Maybe use sorted hash table
-        if _available_numbers is None:
-            _available_numbers = [1 for _ in range(10)]
-            _available_numbers[0] = 0
-
-        def recurse(_sum, _num, _available_numbers):
-            assert _sum > 0 and _num > 0
+        def recurse(sum_, num,  max_number):
+            assert sum_ > 0 and num > 0
 
             # cached values already available
             # TODO(riyansh)
 
             # only one number to fill the sum
-            if _num == 1:
-                if _sum < 10 and _available_numbers[_sum] == 1:
+            if num == 1:
+                if sum_ <= max_number:
                     # only one solution, with only one element
-                    return [[_sum]]
+                    return [[sum_]]
                 else:
                     return None
 
@@ -46,38 +42,36 @@ class KillerSudokuSolver:
             #     return None
 
             # determine the max number to start the search with
-            # if _sum < 9, then _sum - 1
-            # (_sum - 1 because there are atleast 2 numbers that need to 
-            # add up to sum (_num > 1))
-            # if not, then 9
-            max_search_number = _sum - 1 if _sum < 9 else 9
+            max_search_number = max_number
+
+            # determine the min number to stop the search at
+            # for example, starting at 9, you should end at 5
+            # because when try for 4, the complement search will
+            # start at 5 again, which will make for duplicate work
+            min_search_number = ceil(max_search_number/2)
 
             # list of all possible solutions
             possible_solutions = []
 
             # TODO(riyansh): optimize, maybe use sorted hash table as above
-            for i in range(max_search_number, 0, -1):
-                if _available_numbers[i] == 1:
-                    # i is available, try to find solutions
-                    new_available_numbers = copy.copy(_available_numbers)
-                    new_available_numbers[i] = 0
-                    remaining_solutions = recurse(_sum - i, _num - 1, new_available_numbers)
+            for i in range(max_search_number, min_search_number-1, -1):
+                remaining_solutions = recurse(sum_ - i, num - 1, i-1)
 
-                    # we have a solution, possibly multiple solutions
-                    if remaining_solutions:
-                        for remaining_solution in remaining_solutions:
-                            solution = [i]
-                            solution.extend(remaining_solution)
-                            possible_solutions.append(solution)
+                # we have a solution, possibly multiple solutions
+                if remaining_solutions:
+                    for remaining_solution in remaining_solutions:
+                        solution = [i]
+                        solution.extend(remaining_solution)
+                        possible_solutions.append(solution)
 
-                    # exhaust the current number, so as to try only
-                    # for numbers smaller than the current number.
-                    # This results in only a single permutation
-                    # (the one in descending order) to be present in
-                    # the possible_solutions. 
-                    # For example, [1, 2] will never be a solution to
-                    # the call recurse(3, 2), only [2, 1]
-                    _available_numbers[i] = 0
+                # exhaust the current number, so as to try only
+                # for numbers smaller than the current number.
+                # This results in only a single permutation
+                # (the one in descending order) to be present in
+                # the possible_solutions.
+                # For example, [1, 2] will never be a solution to
+                # the call recurse(3, 2), only [2, 1]
+                # available_numbers[i] = 0
                         
             # empty solutions list means no solutions
             if not possible_solutions:
@@ -85,7 +79,7 @@ class KillerSudokuSolver:
 
             return possible_solutions
 
-        return recurse(_sum, _num, _available_numbers)
+        return recurse(sum_, num, 9)
 
 def factorize_time(n, num_factors, N):
     ks = KillerSudokuSolver()
@@ -93,7 +87,7 @@ def factorize_time(n, num_factors, N):
     for i in range(N):
         ks.get_factors(n, num_factors)
     te = time()
-    print((te - ts) * 1000.0 / N, "ms")
+    print((te - ts) * 1000000.0 / N, "us")
     print(ks.get_factors(n, num_factors))
 
 def find_most_difficult_factorization():
